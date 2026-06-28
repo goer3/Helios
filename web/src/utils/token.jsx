ad4ab531c5ca205c7c473bcc252b94ffda0a4d64
DK <1214966109@qq.com>
2026-06-29 01:05:46 +0800

    前后端重构

--- web/src/utils/token.jsx
+++ web/src/utils/token.jsx
@@ -1,14 +1,14 @@
 // 获取本地存储中的 token
 const GetToken = () => {
   const token = localStorage.getItem('token');
-  const expireTimeStr = localStorage.getItem('expire-time');
+  const expireTimeStr = localStorage.getItem('token-expire-time');
 
   if (!expireTimeStr || !token) {
     localStorage.clear();
     return null;
   }
 
-  // expire-time 格式为 YYYY-MM-DD HH:MM:SS
+  // token-expire-time 格式为 YYYY-MM-DD HH:MM:SS
   if (new Date(expireTimeStr) < new Date()) {
     localStorage.clear();
     return null;
@@ -20,7 +20,7 @@ const GetToken = () => {
 // 保存 token 到本地存储
 const SaveToken = (token, expireTime) => {
   localStorage.setItem('token', token);
-  localStorage.setItem('expire-time', expireTime);
+  localStorage.setItem('token-expire-time', expireTime);
 };
 
 export { GetToken, SaveToken };
